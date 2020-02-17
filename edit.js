ca821f6dc488a42b7a49ec6970325625b0b23e6a
Kevinalex1223 <61147821+Kevinalex1223@users.noreply.github.com>
2020-02-17 15:20:14 +0100

    Tarzan test 1

--- edit.js
+++ edit.js
@@ -1,11 +1,11 @@
 function tarzan() {
+  img = document.createElement('img')
+  img.src = '/pngguru.com.png'
+  img.className = 'tarzan';
+
+  document.body.appendChild(img)
+  
   document.onclick = () => {
-    img = document.createElement('img')
-    img.src = '/pngguru.com.png'
-    img.className = 'tarzan';
-    
-    document.body.appendChild(img)
-    
     img.style.top = '500px';
   }
 }
