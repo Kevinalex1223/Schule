26cf2a04a27ceeb08b70a37e6b93f7fa2bedc387
Kevinalex1223 <61147821+Kevinalex1223@users.noreply.github.com>
2020-02-17 15:18:33 +0100

    Tarzan animation

--- edit.js
+++ edit.js
@@ -2,8 +2,11 @@ function tarzan() {
   document.onclick = () => {
     img = document.createElement('img')
     img.src = '/pngguru.com.png'
+    img.className = 'tarzan';
     
     document.body.appendChild(img)
+    
+    img.style.top = '500px';
   }
 }
 
